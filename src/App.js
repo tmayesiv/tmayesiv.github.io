b51e98283d554a51c665226cef273483edf557f4
Thorpe Mayes <tmayesiv@gmail.com>
2025-02-17 11:13:39 -0600

    New branch, CSS changes mostly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import TemperatureCard from "./components/TemperatureCard";
+import BigCard from "./components/BigCard";
 import "./App.css";
 
 function App(){
     return (
         <div className="app-container">
             <div className="grid-container">
-
-                <TemperatureCard title ="Temperature" temperature={25} unit="C"/>
+                <div class="col">
+                    <TemperatureCard title ="Temperature" temperature={25} unit="C"/>
+                    <BigCard title ="Wind" value={12} unit="SW"/>
+                </div>
+                <div class="col">
+                    <BigCard title ="Precipitation" value={10} unit="%"/>
+                    <BigCard title ="Daylight" value={50} unit="%"/>
+                </div>
                 
             </div>
         </div>
--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./TemperatureCard.css";
 
-function TemperatureCard({ title, temperature, unit}) {
+function BigCard({ title, value, unit}) {
     return (
         <div className="temperature-card">
             <h3 className="card-title">{title}</h3>
             <div className="card-content">
                 <img src="/images/sun_temp_icon.svg" alt="sun icon" className="weather-icon" />
                 <p className="temp-value">
-                    {temperature}°{unit}
+                    {value}{unit}
                 </p>
             </div>
         </div>
     );
 }
 
-export default TemperatureCard;
+export default BigCard;
